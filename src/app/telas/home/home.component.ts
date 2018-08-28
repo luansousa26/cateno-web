7fc0d6dab69091fbdf969c79a4c6abc186c57ffa
Luan Sousa <luan26@uninove.edu.br>
2018-08-28 00:00:31 -0300

    Alterações no front

--- src/app/telas/home/home.component.ts
+++ src/app/telas/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('container', [
+      state('active', style({})),
+      transition('inactive => active', [
+        animate(3000, keyframes([
+          style({
+            marginLeft: '-10%'
+          }),
+        ]))
+      ]),
+    ]),
+  ]
 })
+
 export class HomeComponent implements OnInit {
 
+  currentState: any;
   constructor() { }
 
   ngOnInit() {
+    this.currentState = 'inactive';
+    setTimeout(() => {
+      this.currentState = 'active';
+    }, 500);
   }
 
 }
