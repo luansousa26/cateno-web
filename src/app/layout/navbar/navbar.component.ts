99195d28a6f7739f125c895d0f3a85aed8a7af38
unknown <Stefanini@STFALXC039479-W.stefanini.dom>
2018-08-28 16:29:35 -0300

    Alterações

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -13,19 +13,32 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
   mostrarMenu() {
-    if(this.menuvisivel === false) {
-      document.getElementById('menu-hide').style.background = 'rgba(163, 206, 34, 0.541)';
-      document.getElementById('menu-hide').style.height = '200px';
-      document.getElementById('menu-hide').style.transition = 'height 4s ease-in';
-      this.menuvisivel = true;
-    }else {
-      document.getElementById('menu-hide').style.background = 'rgba(163, 206, 34, 0.541)';
-      document.getElementById('menu-hide').style.height = '2px';
-      document.getElementById('menu-hide').style.transition = 'height 4s ease-out';
-      this.menuvisivel = false;
-      setTimeout(() => {
-        document.getElementById('menu-hide').style.background = 'white';
-      }, 4000);
+    if (this.menuvisivel === false) {
+      this.abrirMenu();
+    } else {
+      this.fecharMenu();
     }
+  }
+
+  abrirMenu() {
+    document.getElementById('menu-hide').style.background = 'rgba(163, 206, 34, 0.541)';
+    document.getElementById('menu-hide').style.height = '200px';
+    document.getElementById('menu-hide').style.transition = 'height 4s ease-in';
+    setTimeout(() => {
+      document.getElementById('menus-responsividade').style.display = 'block';
+    }, 4000);
+    this.menuvisivel = true;
+  }
+  fecharMenu() {
+    document.getElementById('menu-hide').style.background = 'rgba(163, 206, 34, 0.541)';
+    document.getElementById('menu-hide').style.height = '2px';
+    document.getElementById('menu-hide').style.transition = 'height 4s ease-out';
+    this.menuvisivel = false;
+    setTimeout(() => {
+      document.getElementById('menus-responsividade').style.display = 'none';
+    }, 900);
+    setTimeout(() => {
+      document.getElementById('menu-hide').style.background = 'white';
+    }, 4000);
   }
 }
