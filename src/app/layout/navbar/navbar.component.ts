bc4257c44845a2c821fabaa0c463b055593c9a17
unknown <Stefanini@STFALXC039479-W.stefanini.dom>
2018-08-28 10:04:41 -0300

    Alteração

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  menuvisivel = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  mostrarMenu() {
+    if(this.menuvisivel === false) {
+      document.getElementById('menu-hide').style.background = 'rgba(163, 206, 34, 0.541)';
+      document.getElementById('menu-hide').style.height = '200px';
+      document.getElementById('menu-hide').style.transition = 'height 4s ease-in';
+      this.menuvisivel = true;
+    }else {
+      document.getElementById('menu-hide').style.background = 'rgba(163, 206, 34, 0.541)';
+      document.getElementById('menu-hide').style.height = '2px';
+      document.getElementById('menu-hide').style.transition = 'height 4s ease-out';
+      this.menuvisivel = false;
+      setTimeout(() => {
+        document.getElementById('menu-hide').style.background = 'white';
+      }, 4000);
+    }
+  }
 }
