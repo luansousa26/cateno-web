7e52bc40ae0c3a4f6a0ffaf5ac3f7fce5a33feb7
Luan Sousa <luan26@uninove.edu.br>
2018-08-30 16:57:57 -0300

    validação de cpf funcionando

--- src/app/layout/solicitar-cartao/solicitar-cartao.component.ts
+++ src/app/layout/solicitar-cartao/solicitar-cartao.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DadosPessoais } from '../../shared/modulos-compartilhados/models/dados-pessoais.model';
+import { Enums } from '../../shared/enums/enums';
+import { Validacoes } from '../../shared/validacoes/validacoes';
 
 @Component({
   selector: 'app-solicitar-cartao',
@@ -8,9 +10,10 @@ import { DadosPessoais } from '../../shared/modulos-compartilhados/models/dados-
 })
 export class SolicitarCartaoComponent implements OnInit {
 
-  cpf: string;
   dadosPessoais: DadosPessoais;
   nomeCartao: string;
+  estadoCpf = true;
+
   constructor() {
     this.dadosPessoais = new DadosPessoais();
   }
@@ -18,42 +21,45 @@ export class SolicitarCartaoComponent implements OnInit {
   ngOnInit() {
   }
   apenasCaracteres(teclaPressionada: KeyboardEvent) {
-    const regexLetras = new RegExp('[a-zA-Z ]');
     this.preencheNomeCartao();
-    return regexLetras.test(teclaPressionada.key) ? true : false;
+    return new RegExp(Enums.LETRAS_E_ESPACO).test(teclaPressionada.key) ? true : false;
   }
   validarEmail(email: string) {
-    const regexEmail = new RegExp('/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i');
-    return regexEmail.test(email) ? true : false;
+    return new RegExp(Enums.EMAIL).test(email) ? true : false;
   }
   apenasNumeros(teclaPressionada: KeyboardEvent) {
-    const regexNumeros = new RegExp('^[0-9]*$');
-    return regexNumeros.test(teclaPressionada.key) ? true : false;
+    return new RegExp(Enums.NUMERICO).test(teclaPressionada.key) ? true : false;
   }
+  validarCpf() {
+    if (this.dadosPessoais.cpf !== undefined &&
+      this.dadosPessoais.cpf !== null) {
+      this.dadosPessoais.cpf.length === 14 ?
+        this.estadoCpf = Validacoes.validarCpf(this.dadosPessoais.cpf) : false;
+    }
 
+  }
   mascaraCpf(teclaPressionada: KeyboardEvent) {
 
-    if(!this.apenasNumeros(teclaPressionada)) {
+    if (!this.apenasNumeros(teclaPressionada)) {
       return false;
     }
     if (this.dadosPessoais.cpf !== null && this.dadosPessoais.cpf !== undefined) {
       switch (this.dadosPessoais.cpf.length) {
         case 3:
-        this.dadosPessoais.cpf = `${this.dadosPessoais.cpf}.`;
+          this.dadosPessoais.cpf = `${this.dadosPessoais.cpf}.`;
           break;
         case 7:
-        this.dadosPessoais.cpf = `${this.dadosPessoais.cpf}.`;
+          this.dadosPessoais.cpf = `${this.dadosPessoais.cpf}.`;
           break;
         case 11:
-        this.dadosPessoais.cpf = `${this.dadosPessoais.cpf}-`;
+          this.dadosPessoais.cpf = `${this.dadosPessoais.cpf}-`;
           break;
       }
     }
   }
-
   preencheNomeCartao() {
-    if(this.dadosPessoais.nome !== null && this.dadosPessoais.nome !== undefined) {
-      this.dadosPessoais.nome.length < 19? this.nomeCartao = this.dadosPessoais.nome: false;
+    if (this.dadosPessoais.nome !== null && this.dadosPessoais.nome !== undefined) {
+      this.dadosPessoais.nome.length < 19 ? this.nomeCartao = this.dadosPessoais.nome : false;
     }
   }
 }
---
+++ src/app/shared/enums/enums.ts
@@ -0,0 +1,5 @@
+export class Enums{
+    public static LETRAS_E_ESPACO = '[a-zA-Z ]';
+    public static EMAIL = '/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i';
+    public static NUMERICO = '^[0-9]*$';
+}
