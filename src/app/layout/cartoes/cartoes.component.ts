57b5d74203d5f8fbe11a7c349f5e17c783223b3c
Luan Sousa <luan26@uninove.edu.br>
2018-09-06 15:35:56 -0300

    Alteração

--- src/app/layout/cartoes/cartoes.component.ts
+++ src/app/layout/cartoes/cartoes.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { Component, OnInit, HostListener } from '@angular/core';
 @Component({
   selector: 'app-cartoes',
   templateUrl: './cartoes.component.html',
@@ -7,16 +6,60 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 })
 export class CartoesComponent implements OnInit {
 
-  animacao: string = 'false';
+  cardTexto = 'Emissão de meios de pagamento pré-pagos';
+  cadeadoText = 'Inteligência em autorização e fraude';
+  pagamentoText = 'Gestão estratégica de meios de pagamento';
+  private controladorCard = 0;
+  private controladorCadeado = 0;
+  private controladorPagamento = 0;
   constructor() { }
 
   ngOnInit() {
-    this.iniciarAnimacao();
+
   }
-  iniciarAnimacao() {
-    setTimeout(() => {
-      this.animacao = 'true';
-    }, 500);
+  @HostListener('window:scroll', [])
+  private scrollAnimate(): void {
+    console.log(window.pageYOffset);
+
+    if (window.pageYOffset > 320) {
+      document.getElementById('iphone-imagem').style.marginTop = '60px';
+      this.animacaoTextoCard();
+      this.animacaoTextoCadeado();
+      this.animacaoTextoPagamento();
+    }
+    if (window.pageYOffset < 320 || window.pageYOffset === 320) {
+      document.getElementById('iphone-imagem').style.marginTop = '10px';
+    }
+  }
+
+  private animacaoTextoCard(): void {
+    if (this.controladorCard < this.cardTexto.length) {
+      document.getElementById('card-texto').innerHTML += this.cardTexto.charAt(this.controladorCard);
+      this.controladorCard++;
+      setTimeout(() => {
+        this.animacaoTextoCard();
+      }, 90);
+    }
+  }
+
+  private animacaoTextoCadeado(): void {
+    if (this.controladorCadeado < this.cardTexto.length) {
+      document.getElementById('cadeado-texto').innerHTML += this.cadeadoText.charAt(this.controladorCadeado);
+      this.controladorCadeado++;
+      setTimeout(() => {
+        this.animacaoTextoCadeado();
+      }, 90);
+    }
+  }
+
+  private animacaoTextoPagamento(): void {
+    if (this.controladorPagamento < this.cardTexto.length) {
+      document.getElementById('pagamento-texto').innerHTML += this.pagamentoText.charAt(this.controladorPagamento);
+      this.controladorPagamento++;
+      setTimeout(() => {
+        this.animacaoTextoPagamento();
+      }, 90);
+    }
   }
 }
 
--- src/app/telas/home/home.component.ts
+++ src/app/telas/home/home.component.ts
@@ -1,52 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('container', [
-      state('inactive', style({
-        marginLeft: '-100%',
-        opacity: '0'
-      })),
-      transition('inactive => active', [
-        animate(6000, keyframes([
-          style({
-            marginLeft: '0',
-            opacity: '1 '
-          }),
-        ])),
-      ]),
-    ]),
-    trigger('cartoes', [
-      state('inactive', style({
-        marginLeft: '400px',
-        opacity: '0'
-      })),
-      transition('inactive => active', [
-        animate(6000, keyframes([
-          style({
-            marginLeft: '0px',
-            opacity: '1 '
-          }),
-        ])),
-      ]),
-    ]),
-  ]
+  styleUrls: ['./home.component.scss']
 })
 
 export class HomeComponent implements OnInit {
 
-  currentState: any;
   constructor() { }
 
   ngOnInit() {
-    this.currentState = 'inactive';
-    setTimeout(() => {
-      this.currentState = 'active';
-    }, 500);
   }
 
 }
